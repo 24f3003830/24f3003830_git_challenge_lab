81176b36430abc39e72244facba97237640227fa
24f3003830 <24f3003830@ds.study.iitm.ac.in>
2025-09-16 23:16:24 +0530

    Add division.py for division operation

---
+++ division.py
@@ -0,0 +1,6 @@
+a=float(input("Enter the value of numerator: "))
+b=float(input("Enter the value of denominator: "))
+if b != 0:
+    print("The division is: ",a/b)
+else:
+    print("Error: Division by zero is not allowed")
